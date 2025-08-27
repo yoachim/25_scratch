bd76e54794de86177ca6c3886d160a1b9b1f839c
Peter Yoachim <yoachim@uw.edu>
2025-08-27 14:50:04 -0700

    runs in parallel

--- sne_parallel/sne_p.py
+++ sne_parallel/sne_p.py
@@ -14,36 +14,7 @@ def call_single_indx(visits, metric, info, indx, nside):
     return result
 
 
-def run_sne():
-    
-    summary_stats = []
-
-    info = maf.empty_info()
-    info["run_name"] = run_name
-    info["observations_subset"] = subset
-    sl = maf.Slicer(nside=nside)
-    metric = maf.SNNSNMetric()
-
-    sn_array, info = sl(visits_array, metric, info=info, skip_setup=True)
-
-    pm = maf.PlotMoll(info=info)
-
-    fig = pm(sn_array["n_sn"], unit="N SNe to z limit")
-    fig_saver(fig, info=info)
-    fig = pm(sn_array["zlim"], unit="z limit")
-    fig_saver(fig, info=info)
-
-    summary_stats.append(maf.gen_summary_row(info, "sum N SNe", np.nansum(sn_array["n_sn"])))
-    summary_stats.append(maf.gen_summary_row(info, "mean z limit", np.nansum(sn_array["zlim"])))
-    summary_stats.append(maf.gen_summary_row(info, "median z limit", np.nanmedian(sn_array["zlim"])))
-
-    return summary_stats
-
-
 if __name__ == '__main__':
-
-
-   
 
     npool = 4
     nside = 4
@@ -80,7 +51,7 @@ if __name__ == '__main__':
 
     t2 = datetime.datetime.now()
     print("time to run in parallel", t2-t1)
-    # XXX--need to manually update the info dict since we went parallel
+    # XXX--need to manually update the info dict since we went parallel and dropped that
 
     # run classic style
     sl = maf.Slicer(nside=nside)
@@ -91,10 +62,12 @@ if __name__ == '__main__':
     t3 = datetime.datetime.now()
     print("time to run single core", t3-t2)
 
+    import pdb ; pdb.set_trace()
+
     assert np.array_equal(sn_array["n_sn"], result["n_sn"])
     assert np.array_equal(sn_array["zlim"], result["zlim"])
 
-    import pdb ; pdb.set_trace()
+    
 
     # check that things worked properly
 
---
+++ sne_parallel/sne_p2.py
@@ -0,0 +1,71 @@
+import numpy as np
+import healpy as hp
+import rubin_sim.maf_proto as maf
+from multiprocessing import Pool
+import matplotlib.pylab as plt
+import datetime
+
+# Should be able to do this then promote things to be globals?
+visits_array, df, run_name, subset, fig_saver = maf.batch_preamble(
+            observations=None,
+            run_name=None,
+            quick_test=False,
+            fig_saver=None,
+        )
+
+good = np.where(visits_array["night"] < 365)
+visits_array = visits_array[good]
+slicer = maf.Slicer(nside=4)
+slicer.setup_slicer(visits_array)
+metric = maf.SNNSNMetric()
+
+
+def call_single_indx(indx):
+    result = slicer(visits_array, metric, info=None, indx=[indx], skip_setup=True)
+    return result
+
+
+def launch_jobs(nside=4, num_jobs=3):
+    nmap = hp.nside2npix(nside)
+
+    with Pool(processes=num_jobs) as pool:
+        result = pool.map(call_single_indx, range(nmap))
+
+    #pool = Pool(processes=num_jobs)
+    #jobs = [pool.apply_async(call_single_indx, [[indx]]) for indx in range(nmap)]
+    #result = [res.get() for res in jobs]
+    result = np.concatenate(result)
+    pool.close()
+    return result
+
+
+if __name__ == '__main__':
+    nside = 4
+
+    t1 = datetime.datetime.now()
+
+    parallel_results = launch_jobs()
+    
+    info = maf.empty_info()
+    info["run_name"] = run_name
+    info["observations_subset"] = "Year 1"
+
+    t2 = datetime.datetime.now()
+    print("time to run in parallel", t2-t1)
+    # XXX--need to manually update the info dict since we went parallel and dropped that
+
+    # run classic style
+    sl = maf.Slicer(nside=nside)
+    metric = maf.SNNSNMetric()
+
+    sn_array, info = sl(visits_array, metric, info=info)
+
+    t3 = datetime.datetime.now()
+    print("time to run single core", t3-t2)
+
+    import pdb ; pdb.set_trace()
+
+    assert np.allclose(sn_array["n_sn"], parallel_results["n_sn"], equal_nan=True)
+    assert np.allclose(sn_array["zlim"], parallel_results["zlim"], equal_nan=True)
+
+
